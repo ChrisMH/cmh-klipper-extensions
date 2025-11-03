4c083dfe34f49176d5e934fdd7faa64f9687964e
Chris Hogan <hogancm@gmail.com>
2025-11-03 10:14:56 -0500

    Add supports_dual_carriage to kinematics

--- kinematics/ratos_hybrid_corexy/ratos_hybrid_corexy.py
+++ kinematics/ratos_hybrid_corexy/ratos_hybrid_corexy.py
@@ -17,11 +17,12 @@ class RatOSHybridCoreXYKinematics:
         if config.has_section('ratos_hybrid_corexy'):
             hcxy_config = config.getsection('ratos_hybrid_corexy')
             self.inverted = hcxy_config.getboolean('inverted', False)
+
         # itersolve parameters
-        self.rails = [stepper.LookupMultiRail(config.getsection('stepper_' + n))
-                      for n in 'xyz']
+        self.rails = [stepper.LookupMultiRail(config.getsection('stepper_' + n)) for n in 'xyz']
         for s in self.rails[0].get_steppers():
             self.rails[1].get_endstops()[0][0].add_stepper(s)
+
         if self.inverted == False:
             self.rails[0].steppers[0].setup_itersolve('corexy_stepper_alloc', b'-')
             if len(self.rails[0].steppers)==2:
@@ -34,12 +35,15 @@ class RatOSHybridCoreXYKinematics:
                 self.rails[0].steppers[1].setup_itersolve('corexy_stepper_alloc', b'-')
             if len(self.rails[0].steppers)>2:
                 raise self.error("Unexpected stepper configuration")
+
         self.rails[1].setup_itersolve('cartesian_stepper_alloc', b'y')
         self.rails[2].setup_itersolve('cartesian_stepper_alloc', b'z')
         ranges = [r.get_range() for r in self.rails]
         self.axes_min = toolhead.Coord(*[r[0] for r in ranges], e=0.)
         self.axes_max = toolhead.Coord(*[r[1] for r in ranges], e=0.)
+
         self.dc_module = None
+        self.supports_dual_carriage = False
         if config.has_section('dual_carriage'):
             dc_config = config.getsection('dual_carriage')
             # dummy for cartesian config users
@@ -64,26 +68,27 @@ class RatOSHybridCoreXYKinematics:
                     self.rails[0], axis=0, active=True)
             dc_rail_1 = idex_modes.DualCarriagesRail(
                     self.rails[3], axis=0, active=False)
-            self.dc_module = idex_modes.DualCarriages(
-                    dc_config, dc_rail_0, dc_rail_1, axis=0)
+            self.dc_module = idex_modes.DualCarriages(dc_config, dc_rail_0, dc_rail_1, axis=0)
+            self.supports_dual_carriage = True
+
         for s in self.get_steppers():
             s.set_trapq(toolhead.get_trapq())
             toolhead.register_step_generator(s.generate_steps)
-        self.printer.register_event_handler("stepper_enable:motor_off",
-                                                    self._motor_off)
+
+        self.printer.register_event_handler("stepper_enable:motor_off", self._motor_off)
+        
         # Setup boundary checks
         max_velocity, max_accel = toolhead.get_max_velocity()
-        self.max_z_velocity = config.getfloat(
-            'max_z_velocity', max_velocity, above=0., maxval=max_velocity)
-        self.max_z_accel = config.getfloat(
-            'max_z_accel', max_accel, above=0., maxval=max_accel)
+        self.max_z_velocity = config.getfloat('max_z_velocity', max_velocity, above=0., maxval=max_velocity)
+        self.max_z_accel = config.getfloat('max_z_accel', max_accel, above=0., maxval=max_accel)
         self.limits = [(1.0, -1.0)] * 3
+
     def get_steppers(self):
         return [s for rail in self.rails for s in rail.get_steppers()]
+
     def calc_position(self, stepper_positions):
         pos = [stepper_positions[rail.get_name()] for rail in self.rails]
-        if (self.dc_module is not None and 'PRIMARY' == \
-                    self.dc_module.get_status()['carriage_1']):
+        if (self.dc_module is not None and 'PRIMARY' == self.dc_module.get_status()['carriage_1']):
             if self.inverted == False:
                 return [pos[3] - pos[1], pos[1], pos[2]]
             else:
@@ -93,6 +98,7 @@ class RatOSHybridCoreXYKinematics:
                 return [pos[0] + pos[1], pos[1], pos[2]]
             else:
                 return [pos[0] - pos[1], pos[1], pos[2]]
+
     def update_limits(self, i, range):
         l, h = self.limits[i]
         # Only update limits if this axis was already homed,
@@ -108,9 +114,11 @@ class RatOSHybridCoreXYKinematics:
                 else:
                     rail = self.rails[axis]
                 self.limits[axis] = rail.get_range()
+
     def note_z_not_homed(self):
         # Helper for Safe Z Home
         self.limits[2] = (1.0, -1.0)
+
     def home_axis(self, homing_state, axis, rail):
         position_min, position_max = rail.get_range()
         hi = rail.get_homing_info()
@@ -123,14 +131,17 @@ class RatOSHybridCoreXYKinematics:
             forcepos[axis] += 1.5 * (position_max - hi.position_endstop)
         # Perform homing
         homing_state.home_rails([rail], forcepos, homepos)
+
     def home(self, homing_state):
         for axis in homing_state.get_axes():
             if self.dc_module is not None and axis == 0:
                 self.dc_module.home(homing_state)
             else:
                 self.home_axis(homing_state, axis, self.rails[axis])
+
     def _motor_off(self, print_time):
         self.limits = [(1.0, -1.0)] * 3
+
     def _check_endstops(self, move):
         end_pos = move.end_pos
         for i in (0, 1, 2):
@@ -140,6 +151,7 @@ class RatOSHybridCoreXYKinematics:
                 if self.limits[i][0] > self.limits[i][1]:
                     raise move.move_error("Must home axis first")
                 raise move.move_error()
+
     def check_move(self, move):
         limits = self.limits
         xpos, ypos = move.end_pos[:2]
@@ -154,6 +166,7 @@ class RatOSHybridCoreXYKinematics:
         z_ratio = move.move_d / abs(move.axes_d[2])
         move.limit_speed(
             self.max_z_velocity * z_ratio, self.max_z_accel * z_ratio)
+
     def get_status(self, eventtime):
         axes = [a for a, (l, h) in zip("xyz", self.limits) if l <= h]
         return {
